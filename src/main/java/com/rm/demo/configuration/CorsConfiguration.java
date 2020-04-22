83b1e4fdaffc89b3208afe64149b52d1818aee87
yumin-dev <yumin.li.2005@gmail.com>
2020-04-22 18:20:44 -0400

    cors configuration for local testing

---
+++ src/main/java/com/rm/demo/configuration/CorsConfiguration.java
@@ -0,0 +1,13 @@
+package com.rm.demo.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer{
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
